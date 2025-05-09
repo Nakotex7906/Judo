8b4dbf87a8a7908d51822c834bb3798b79992bba
BenjaBeroiiza <b.beroiza01@ufromail.cl>
2025-05-09 17:35:57 -0400

    Se mejoro la funcionalidad de los metodos con respecto a los errores en estos

--- src/main/java/org/example/controllerWeb/CompetenciaWebController.java
+++ src/main/java/org/example/controllerWeb/CompetenciaWebController.java
@@ -6,10 +6,7 @@ import org.example.service.CompetenciaService;
 import org.example.service.JudokaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -56,20 +53,27 @@ public class CompetenciaWebController {
     @GetMapping("/competencias/detalle/{id}") //Se agrego un mensaje de error
     public String detalleCompetencia(@PathVariable Long id, Model model) {
         Competencia competencia = competenciaService.buscarPorId(id)
-                .orElseThrow(()-> new RuntimeException("Competencia no encontrada")); //
+                .orElseThrow(()-> new RuntimeException("Competencia no encontrada"));
         return "detalle_competencia"; // Nombre del template HTML
+    }
+
+    /**
+     * Cargar judokas list.
+     *
+     * @return the list
+     */
+    @ModelAttribute("judokas") //con este metodo la lista de judokas se agrega automaticamente, ejemplo metodo mostrarCrearCompetencia
+    public List<Judoka>cargarJudokas(){
+        return judokaService.listarJudokas();
     }
 
     /**
      * Mostrar crear competencia string.
      *
-     * @param model the model
      * @return the string
      */
     @GetMapping("/competencias/crear")
-    public String mostrarCrearCompetencia(Model model) {
-        List<Judoka> judokas = judokaService.listarJudokas();
-        model.addAttribute("judokas", judokas);
+    public String mostrarCrearCompetencia() {
         return "crear_competencia";
     }
 
@@ -81,13 +85,24 @@ public class CompetenciaWebController {
      * @param participantes the participantes
      * @return the string
      */
-    @PostMapping("/competencias/crear")
+    @PostMapping("/competencias/crear") //Se cambio el metodo para validar los parametros de entrada, linea 88 a 98
     public String crearCompetencia(@RequestParam String nombre,
                                    @RequestParam String fecha,
                                    @RequestParam List<Long> participantes) {
+        if(nombre == null || nombre.isEmpty() || fecha == null || fecha.isEmpty()) {
+            return "redirect:/competencias/crear?error=DatosInvalidos";
+        }
+        if (participantes == null || participantes.isEmpty()){
+            return "redirect:/competencias/crear?error=NoParticipantes\n";
+        }
         List<Judoka> judokasSeleccionados = judokaService.buscarPorIds(participantes);
+        if (judokasSeleccionados.isEmpty()){
+            return "redirect:/competencias/crear?error=ParticipantesNoValidos";
+
+        }
         Competencia nueva = new Competencia(nombre, fecha, judokasSeleccionados);
         competenciaService.guardarCompetencia(nueva);
+
         return "redirect:/competencias";
     }
 
