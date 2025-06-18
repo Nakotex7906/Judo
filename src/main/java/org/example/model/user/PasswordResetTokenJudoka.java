f027f534dbfc59319505977dd779c37ebb965e0f
Ignacio Essus <i.essus01@ufromail.cl>
2025-06-18 17:12:20 -0400

    Se cambiaron los getters y setters por la anotacion @Data

--- src/main/java/org/example/model/user/PasswordResetTokenJudoka.java
+++ src/main/java/org/example/model/user/PasswordResetTokenJudoka.java
@@ -1,8 +1,11 @@
 package org.example.model.user;
 
 import jakarta.persistence.*;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 @Entity
 public class PasswordResetTokenJudoka {
 
@@ -23,18 +26,5 @@ public class PasswordResetTokenJudoka {
         return expiryDate.isBefore(LocalDateTime.now());
     }
 
-    // Getters y setters
-    public Long getId() { return id; }
-    public String getToken() { return token; }
-    public void setToken(String token) { this.token = token; }
-
-    public Judoka getJudoka() { return judoka; }
-    public void setJudoka(Judoka judoka) { this.judoka = judoka; }
-
-    public LocalDateTime getExpiryDate() { return expiryDate; }
-    public void setExpiryDate(LocalDateTime expiryDate) { this.expiryDate = expiryDate; }
-
-    public boolean isUsed() { return used; }
-    public void setUsed(boolean used) { this.used = used; }
 }
 
