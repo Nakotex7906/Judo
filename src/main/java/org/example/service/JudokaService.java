86b281d5a5d7c2e2ff55f69110f8fcf636db65cf
Ignacio Essus <i.essus01@ufromail.cl>
2025-07-06 20:10:53 -0400

    Borrar metodos que no se ocupaban

--- src/main/java/org/example/service/JudokaService.java
+++ src/main/java/org/example/service/JudokaService.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.example.model.user.Judoka;
 import org.example.repository.JudokaRepository;
-import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -61,11 +60,6 @@ public class JudokaService {
         return judokaRepository.findAllById(participantes);
     }
 
-    // MODIFICADO: Se añade un método para listar con orden.
-    public List<Judoka> listarJudokas(Sort sort) {
-        return judokaRepository.findAll(sort);
-    }
-
     // MODIFICADO: Se añade un método para buscar por ID.
     public Optional<Judoka> buscarPorId(Long id) {
         return judokaRepository.findById(id);
@@ -76,16 +70,10 @@ public class JudokaService {
         return judokaRepository.findByClubIsNull();
     }
 
-    // MODIFICADO: Se añade el nuevo método para usar la consulta con JOIN FETCH.
-    public Optional<Judoka> findByUsernameWithClub(String username) {
-        return judokaRepository.findByUsernameWithClub(username);
-    }
-
     public void eliminarCuentaJudoka(String username) {
         Optional<Judoka> judoka = findByUsername(username);
         if (judoka.isPresent()) {
             // Eliminar registros relacionados primero
-            // TODO: Implementar lógica para eliminar registros en tablas relacionadas si existen
             judokaRepository.delete(judoka.get());
         } else {
             throw new EntityNotFoundException("No se encontró el judoka con username: " + username);
